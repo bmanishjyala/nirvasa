ab391af4a5e624ed7a7fb897cdfb0ef0689fa9c5
CodeFox <81969897+bmanishjyala@users.noreply.github.com>
2022-05-31 14:17:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+document.getElementById("event0").addEventListener("click", function () {
+  document.getElementById("toggle").innerHTML = ` <div class="attcToggle mt-4">
+    <div class="left">
+        <h3 class="fw-bold">Ayurvedic</h3>
+        <p> Blessed with a rich Ayurvedic legacy, India is privy to the healing secrets of Ayurveda. We are infusing these secret formulations in our products introducing health supplements that are created using Ayurvedic herbs and formulated as per Ayurvedic literature. Our products are traditional remedies that help healing a range of everyday health issues.</p>
+    </div>
+    <div class="right" >
+        <img width="100%" height="100%" alt="promise-tab" class=""
+            src="https://www.nirvasa.com/wp-content/uploads/2021/01/03-Ayurvedic.jpg" />
+    </div>
+</div>`;
+});
+document.getElementById("event1").addEventListener("click", function () {
+  document.getElementById("toggle").innerHTML = ` <div class="attcToggle mt-4">
+    <div class="left">
+        <h3 class="fw-bold">Non-toxic</h3>
+        <p style="text-align: left;">Good for the planet and gentle for humans, our range is free from toxic
+            materials and synthetic ingredients. Following strict product safety policies, we do not use
+            parabens, sulfates, mineral oil, fillers, preservatives, synthetic colours, and more while
+            manufacturing.</p>
+    </div>
+    <div class="right" >
+        <img width="100%" height="100%" alt="promise-tab" class=""
+            src="https://www.nirvasa.com/wp-content/uploads/2021/01/02-Non-Toxic.jpg" />
+    </div>
+</div>`;
+});
+document.getElementById("event2").addEventListener("click", function () {
+  document.getElementById("toggle").innerHTML = ` <div class="attcToggle mt-4">
+    <div class="left">
+        <h3 class="fw-bold">Natural Ingredients</h3>
+        <p style="text-align: left;">Having the highest standard of quality control measures in place, we provide our customers not just products but solutions that are weaved without using any synthetic materials but natural ingredients sourced from different parts of the country – locations where the best of them are found.</p>
+    </div>
+    <div class="right" >
+        <img width="100%" height="100%" alt="promise-tab" class=""
+            src="https://www.nirvasa.com/wp-content/uploads/2021/01/01-all-natural.jpg" />
+    </div>
+</div>`;
+});
+document.getElementById("event3").addEventListener("click", function () {
+  document.getElementById("toggle").innerHTML = ` <div class="attcToggle mt-4">
+    <div class="left">
+        <h3 class="fw-bold">No Side Effects</h3>
+        <p style="text-align: left;">Our conscientious manufacturing process and use of natural ingredients transcends into quality products, which help our customers solve their health problems. Proudly, we can say that none of our customers has ever reported any side-effect after consuming a product, which is the true testimony to our product quality.</p>
+    </div>
+    <div class="right" >
+        <img width="100%" height="100%" alt="promise-tab" class=""
+            src="https://www.nirvasa.com/wp-content/uploads/2021/01/04-No-Side-Effects.jpg" />
+    </div>
+</div>`;
+});
+document.getElementById("event4").addEventListener("click", function () {
+  document.getElementById("toggle").innerHTML = ` <div class="attcToggle mt-4">
+    <div class="left">
+        <h3 class="fw-bold">Safe & Certified</h3>
+        <p style="text-align: left;">All our products are manufactured in GMP certified facilities and are certified safe for consumption by FSSAI. From ingredients to manufacturing, we take precise care of all factors ranging from hygiene, sustainable manufacturing practices, and use of organically harvested ingredients.</p>
+    </div>
+    <div class="right" >
+        <img width="100%" height="100%" alt="promise-tab" class=""
+            src="https://www.nirvasa.com/wp-content/uploads/2021/01/05-Safe-Certified-5.jpg" />
+    </div>
+</div>`;
+});
